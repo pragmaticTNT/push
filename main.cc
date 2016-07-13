b10c109bc9aca015f2049b239d6ffeb6ca0a99d2
noodles <pragmaticTNT@gmail.com>
2016-07-12 20:22:19 -0700

    Removed from main.cc.

--- main.cc
+++ main.cc
@@ -6,96 +6,6 @@
 
 #include "push.hh"
 
-//class Pusher : public Robot {
-//    private:
-//        typedef enum {
-//            S_PUSH = 0,
-//            S_BACKUP_LONG,
-//            S_BACKUP_SHORT,
-//            S_TURN,
-//            S_COUNT
-//        } control_state_t;
-//
-//        static const float PUSH, BACKUP, TURNMAX;
-//        static const float SPEEDX, SPEEDA;
-//        static const float maxspeedx, maxspeeda;
-//
-//        float timeleft;
-//        control_state_t state;
-//        float speedx, speeda;
-//        float epsilon;   // spawn away from wall
-//        int turnRight;
-//
-//    public:
-//    // constructor
-//    Pusher( World& world, float epsilon ) : 
-//        epsilon(epsilon),
-//        Robot( world, 
-//            drand48() * (world.width - 2*epsilon) + epsilon,
-//            drand48() * (world.height - 2*epsilon) + epsilon, 
-//            -M_PI + drand48() * 2.0*M_PI), 
-//        state( S_TURN ),
-//        timeleft( drand48() * TURNMAX ),
-//        speedx( 0 ),
-//        speeda( 0 ) {
-//        turnRight = drand48() < 0.5 ? 1 : -1; 
-//        }
-//
-//    virtual void Update( float timestep ) {
-//        // ===> IMPLEMENT ROBOT BEHAVIOUR WITH A LITTLE STATE MACHINE
-//        b2Vec2 here = body->GetWorldCenter();
-//        float currentLightIntensity = lightCTRL->GetIntensity(here.x, here.y);
-//        // count down to changing control state
-//        timeleft -= timestep;
-//
-//        // force a change of control state
-//        if( state == S_PUSH && 
-//            (currentLightIntensity < lightCTRL->GetSmallLight() || GetBumperPressed()) ){
-//            timeleft = 0.0; // end pushing right now
-//            speedx = 0;
-//            speeda = 0;
-//        }
-//
-//        if( timeleft <= 0 ) // time to change to another behaviour
-//            switch( state ) {
-//                case S_PUSH:
-//                    //std::cout << "In state: BACKUP << std::endl;
-//                    state = currentLightIntensity < lightCTRL->GetSmallLight() ? 
-//                        S_BACKUP_LONG : S_BACKUP_SHORT;
-//                    timeleft = currentLightIntensity < lightCTRL->GetSmallLight() ?
-//                        0 : BACKUP;
-//                    speedx = -SPEEDX;
-//                    speeda = 0;	          
-//                    break;
-//                case S_BACKUP_LONG: 
-//                    if( currentLightIntensity > lightCTRL->GetLargeLight() || fabs(timeleft) > 10 * BACKUP )
-//                        state = S_BACKUP_SHORT;
-//                    else if( currentLightIntensity < lightIntensity )
-//                        speedx = -speedx;
-//                    break;
-//                case S_BACKUP_SHORT: 
-//                    //std::cout << "In state: TURN" << std::endl;
-//                    state = S_TURN;
-//                    timeleft = drand48() * TURNMAX;
-//                    speedx = 0;
-//                    speeda = turnRight * SPEEDA;	    
-//                    break;
-//                case S_TURN: 
-//                    //std::cout << "In state: PUSH" << std::endl;
-//                    state = S_PUSH;
-//                    timeleft = PUSH;
-//                    speedx = SPEEDX;
-//                    speeda = 0;	 
-//                    break;
-//                default:
-//                    std::cout << "invalid control state: " << state << std::endl;
-//                    exit(1);
-//            }
-//        SetSpeed( speedx, 0, speeda );
-//        lightIntensity = currentLightIntensity;
-//    }
-//}; // class Pusher
-
 // static members
 const float Pusher::PUSH = 10.0; // seconds
 const float Pusher::BACKUP = 0.1;
@@ -151,36 +61,9 @@ int main( int argc, char* argv[] ){
         }
     }
 
-//    std::vector<Goal*> goals;
-//    std::vector<Box*> boxes;    
-//    std::vector<Robot*> robots;
-//
-//    // One time sort the goals by x --- then y --- coordinate please
-//    goals.push_back( new Goal(3, 3, Box::size/2) );        
-//    goals.push_back( new Goal(1, 2, Box::size/2) );        
-//    //goals.push_back( new Goal(2, 2, Box::size/2) );        
-//    //goals.push_back( new Goal(4, 4, Box::size/2) );        
-//    //goals.push_back( new Goal(4, 1, Box::size/2) );        
-//
-//    LightController lightCTRL(goals, 0.1, 0.5, 0.6);
-//
-//    GuiWorld world( WIDTH, HEIGHT, Robot::size, lightCTRL );
-//    std::cout << "Starting " << WIDTH << "x" << HEIGHT << " World... robots: " << ROBOTS << " boxes: " << BOXES << std::endl;
-//
-//    for( int i=0; i<BOXES; i++ )
-//        boxes.push_back( new Box( world ) );
-//
-//    for( int i=0; i<ROBOTS; i++ )
-//        robots.push_back( new Pusher( world, Box::size ) );
-//    Robot::lightCTRL = &lightCTRL;
-
     GuiWorld world( WIDTH, HEIGHT, Robot::size, ROBOTS, BOXES);
     /* Loop until the user closes the window */
     while( !world.RequestShutdown() ) {
-        // if( ! GuiWorld::paused )
-        // for( int i=0; i<ROBOTS; i++ )
-        //     robots[i]->Update( timeStep );
-        // lightCTRL.Update(goals, boxes);
         world.Step( timeStep );
     }
 
